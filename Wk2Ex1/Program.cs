20bba446e87fb3294057d26b71687d40df5fb49b
n0488629 <n0488629@amarillocollege.com>
2025-01-23 16:52:54 -0600

    fixed
    messed up repos

--- Wk2Ex1/Program.cs
+++ Wk2Ex1/Program.cs
@@ -15,25 +15,47 @@ namespace Wk2Ex1
             // Convert user input to double variable
             double grade = Convert.ToDouble(Console.ReadLine());
 
-            switch (grade)
-            {
-                case double i when i >= 89.5:
-                    Console.WriteLine("The letter grade for score " + grade + " is: A");
-                    break;
-                case double i when i >= 79.5:
-                    Console.WriteLine("The letter grade for score " + grade + " is: B");
-                    break;
-                case double i when i >= 69.5:
-                    Console.WriteLine("The letter grade for score " + grade + " is: C");
-                    break;
-                case double i when i >= 59.5:
-                    Console.WriteLine("The letter grade for score " + grade + " is: D");
-                    break;
-                default:
-                    Console.WriteLine("The letter grade for score " + grade + " is: F");
-                    break;
-            }
-            Console.ReadLine();
+            // Start switch with variable grade
+switch (grade)
+
+{
+    // assigns grade to variable i, then compared to letter scale
+    case double i when i >= 89.5:
+        // Print out letter and numerical grade on screen
+        Console.WriteLine("The letter grade for score " + grade + " is: A");
+        // Force exits the case/switch
+        break;
+
+    // assigns grade to variable i, then compared to letter scale
+    case double i when i >= 79.5:
+        // Print out letter and numerical grade on screen
+        Console.WriteLine("The letter grade for score " + grade + " is: B");
+        // Force exits the case/switch
+        break;
+
+    // assigns grade to variable i, then compared to letter scale
+    case double i when i >= 69.5:
+        // Print out letter and numerical grade on screen
+        Console.WriteLine("The letter grade for score " + grade + " is: C");
+        // Force exits the case/switch
+        break;
+
+    // assigns grade to variable i, then compared to letter scale
+    case double i when i >= 59.5:
+        // Print out letter and numerical grade on screen
+        Console.WriteLine("The letter grade for score " + grade + " is: D");
+        // Force exits the case/switch
+        break;
+
+    // default option if no cases are true
+    default:
+        // Print out letter and numerical grade on screen
+        Console.WriteLine("The letter grade for score " + grade + " is: F");
+        // Force exits the case/switch
+        break;
+}
+// Pause screen for user
+Console.ReadLine();
 
         }
     }
